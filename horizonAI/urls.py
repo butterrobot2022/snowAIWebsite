b61b2252c5989dbb6eda51f5c58f9606f59100d6
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-08-07 12:13:16 +0200

    new commit

--- horizonAI/urls.py
+++ horizonAI/urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
     path('all_trades', views.all_trades, name='all_trades'),
     path('trade_analysis/<int:identifier>', views.trade_analysis, name='trade_analysis'),
     path('forgot_password', views.forgot_password, name='forgot_password'),
-    path('send_code/<str:to_email>', views.send_code, name='send_code'), 
     path('save_new_password/<str:email>/<str:new_password>', views.save_new_password, name='save_new_password'),
     path('journal/retrieve', views.retrieve_journal, name='get_journal'),
     path('trades/all', views.get_all_trades, name='get_all_trades'),
